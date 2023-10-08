00efeed7f6fa7b357c8ef24bd1d88a549f401dc0
Webster Dev <webster.rafael2014@gmail.com>
2023-10-08 07:27:27 -0300

    NEW

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ function toggleMode() {
     }
 
     }
+
+    window.sr = ScrollReveal({ reset: true});
+    sr.reveal('.project-h1', {
+         duration: 2000,
+         rotate: {x:0, y: 80, z:0}
+    });
+
+    ScrollReveal().reveal('.lista-1', { duration: 2000, // duração da animação em milissegundos
+    origin: 'left', // de onde a animação deve vir ('left' para da esquerda para a direita)
+    distance: '200px', // distância do deslocamento
+    delay: 200 });
+
+    ScrollReveal().reveal('.lista-2', { duration: 3000, // duração da animação em milissegundos
+    origin: 'right', // de onde a animação deve vir ('left' para da esquerda para a direita)
+    distance: '200px', // distância do deslocamento
+    delay: 200 });
+
+    ScrollReveal().reveal('.social-1', { duration: 2000, // duração da animação em milissegundos
+    origin: 'bottom', // de onde a animação deve vir ('left' para da esquerda para a direita)
+    distance: '200px', // distância do deslocamento
+    delay: 200 });
